38cf116876d78df69970cd5fecb7cb57ab09572e
Todd Wostrel <todd@palakey.com>
2016-04-02 11:34:03 -0500

    Tie sidebar into authentication

---
+++ modules/core/client/controllers/sidebar.client.controller.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('core')
+    .controller('SidebarController', SidebarController);
+
+  SidebarController.$inject = ['$scope', '$state', 'Authentication', 'menuService'];
+
+  function SidebarController($scope, $state, Authentication, menuService) {
+    var vm = this;
+
+    vm.accountMenu = menuService.getMenu('account').items[0];
+    vm.authentication = Authentication;
+    vm.isCollapsed = false;
+    vm.menu = menuService.getMenu('topbar');
+
+    $scope.$on('$stateChangeSuccess', stateChangeSuccess);
+
+    function stateChangeSuccess() {
+      // Collapsing the menu after navigation
+      vm.isCollapsed = false;
+    }
+  }
+}());
