9ccf620995f5243cfaff88bd06506ad3a21eb49a
Todd Wostrel <todd@palakey.com>
2016-04-07 10:50:34 -0500

    Remove unneeded module

--- modules/compensation/client/compensations.client.module.js
+++
@@ -1,7 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('compensations', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('compensations.services');
-  app.registerModule('compensations.routes', ['ui.router', 'core.routes', 'compensations.services']);
-}(ApplicationConfiguration));
